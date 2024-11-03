53df432a92a1408a9561a24b044795d8e27399ba
Jose Zerpa <zjose88@gmail.com>
2024-11-02 23:28:42 -0300

    Refactor Cloud Function to handle CloudEvent and log storage object details

--- function.go
+++ function.go
@@ -1,19 +1,43 @@
-package main
+// Package helloworld provides a set of Cloud Functions samples.
+package helloworld
 
 import (
 	"context"
+	"fmt"
 	"log"
+	"time"
+
+	"github.com/GoogleCloudPlatform/functions-framework-go/functions"
+	"github.com/cloudevents/sdk-go/v2/event"
 )
 
-// GCSEvent is the payload of a GCS event. Please refer to the docs for
-// additional information regarding GCS events.
-type GCSEvent struct {
-	Bucket string `json:"bucket"`
-	Name   string `json:"name"`
+func init() {
+	functions.CloudEvent("HelloStorage", helloStorage)
 }
 
-// handler prints a message when a file is changed in a Cloud Storage bucket.
-func handler(ctx context.Context, e GCSEvent) error {
-	log.Printf("Processing file: %s in bucket: %s", e.Name, e.Bucket)
+// StorageObjectData contains metadata of the Cloud Storage object.
+type StorageObjectData struct {
+	Bucket         string    `json:"bucket,omitempty"`
+	Name           string    `json:"name,omitempty"`
+	Metageneration int64     `json:"metageneration,string,omitempty"`
+	TimeCreated    time.Time `json:"timeCreated,omitempty"`
+	Updated        time.Time `json:"updated,omitempty"`
+}
+
+// helloStorage consumes a CloudEvent message and logs details about the changed object.
+func helloStorage(ctx context.Context, e event.Event) error {
+	log.Printf("Event ID: %s", e.ID())
+	log.Printf("Event Type: %s", e.Type())
+
+	var data StorageObjectData
+	if err := e.DataAs(&data); err != nil {
+		return fmt.Errorf("event.DataAs: %v", err)
+	}
+
+	log.Printf("Bucket: %s", data.Bucket)
+	log.Printf("File: %s", data.Name)
+	log.Printf("Metageneration: %d", data.Metageneration)
+	log.Printf("Created: %s", data.TimeCreated)
+	log.Printf("Updated: %s", data.Updated)
 	return nil
 }
